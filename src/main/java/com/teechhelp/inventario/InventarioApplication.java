229c886014a3c6d85f69e99432c6568e60de3ade
brayden1308 <braydenvargas13@GMAIL.COM>
2025-05-10 22:25:19 -0500

    Corrijo ubicación del main y aseguro escaneo de controlador

--- src/main/java/com/teechhelp/inventario/InventarioApplication.java
+++ src/main/java/com/teechhelp/inventario/InventarioApplication.java
@@ -1,11 +1,12 @@
-package com.teechhelp.inventario;
+package com.teechhelp.inventario; // Asegúrate de que este paquete sea correcto
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // Esta anotación habilita la configuración automática de Spring Boot
 public class InventarioApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(InventarioApplication.class, args);
+		SpringApplication.run(InventarioApplication.class, args); // Inicia la aplicación
 	}
 }
--- src/test/java/com/InventarioApplicationTests.java
+++ src/test/java/com/InventarioApplicationTests.java
@@ -1,4 +1,4 @@
-package com;
+package com.teechhelp.inventario;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +9,4 @@ class InventarioApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
