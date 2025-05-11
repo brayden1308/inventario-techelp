34870d6356c9b7ce5d31aa9245b78507127e49ae
brayden1308 <braydenvargas13@GMAIL.COM>
2025-05-10 21:12:06 -0500

    Corrijo ubicación de clase principal

--- src/main/java/com/teechhelp/inventario/InventarioApplication.java
+++ src/main/java/com/teechhelp/inventario/InventarioApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventarioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(InventarioApplication.class, args);
 	}
-
 }
