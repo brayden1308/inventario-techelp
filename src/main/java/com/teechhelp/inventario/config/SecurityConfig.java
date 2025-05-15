04ca2283175b1b650cb4c601ebc9efb1f6f93652
brayden1308 <braydenvargas13@GMAIL.COM>
2025-05-15 09:59:48 -0500

    Corrige estructura de paquetes y carpetas

--- src/main/java/com/teechhelp/inventario/config/SecurityConfig.java
+++ src/main/java/com/teechhelp/inventario/config/SecurityConfig.java
@@ -2,8 +2,8 @@ package com.teechhelp.inventario.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
 public class SecurityConfig {
@@ -11,9 +11,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Desactiva CSRF para pruebas
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Permitir TODAS las rutas sin login
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable())
                 );
 
         return http.build();
