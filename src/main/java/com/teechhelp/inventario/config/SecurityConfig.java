dcb2d885284b49927e530fc964cf9a674f7eb21d
brayden1308 <braydenvargas13@GMAIL.COM>
2025-05-15 12:04:34 -0500

    Corrige estructura de paquetes y carpetas

--- src/main/java/com/teechhelp/inventario/config/SecurityConfig.java
+++ src/main/java/com/teechhelp/inventario/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://inventario-techelp.onrender.com"));
+        configuration.setAllowedOrigins(List.of("https://inventario-techhelp.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
